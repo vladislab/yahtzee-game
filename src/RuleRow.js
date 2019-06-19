767d3c8b47d6951acffff1bbb1a177dfde02449e
Viet Nguyen <vmnguyen@ryerson.ca>
2019-06-19 01:57:51 -0400

    Disallow Score Line Reuse

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
-import './RuleRow.css'
+import React, { Component } from "react";
+import "./RuleRow.css";
 
 class RuleRow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isDisabled: false };
+  }
+  handleClick = () => {
+    this.setState({ isDisabled: true });
+    this.props.doScore();
+  };
   render() {
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
+      <tr
+        className={`RuleRow RuleRow-${
+          this.state.isDisabled ? `disabled` : `active`
+        }`}
+        onClick={this.handleClick}
+      >
         <td className="RuleRow-name">{this.props.name}</td>
         <td className="RuleRow-score">{this.props.score}</td>
       </tr>
-    )
+    );
   }
 }
 
