af189fec3f7c288d6934fcc83b31e1b6cc00911f
Viet Nguyen <vmnguyen@ryerson.ca>
2019-06-26 16:37:18 -0400

    Updates

---
+++ precache-manifest.657e338da311b81caaf3490e0da0dbd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8406f3af2553fbdefc36f79deb00c039",
+    "url": "/yahtzee-game/index.html"
+  },
+  {
+    "revision": "c4dd3f391773691d515a",
+    "url": "/yahtzee-game/static/css/main.3a7fa0b0.chunk.css"
+  },
+  {
+    "revision": "5c611510bb7444aa3e38",
+    "url": "/yahtzee-game/static/js/2.03fb47ab.chunk.js"
+  },
+  {
+    "revision": "c4dd3f391773691d515a",
+    "url": "/yahtzee-game/static/js/main.5d090bab.chunk.js"
+  },
+  {
+    "revision": "310e3cf6a4ac4bf65dbe",
+    "url": "/yahtzee-game/static/js/runtime~main.6f837cd2.js"
+  }
+]);
